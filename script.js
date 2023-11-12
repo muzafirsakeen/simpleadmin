8b45550b944840cdcd05d04282e0109c8a840f7c
MUSAFAR SAKEEN <musafarshakeel@gmail.com>
2023-11-12 21:57:37 +0530

    updated the api link due to security issue

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     async function fetchData() {
         try {
-            const response = await fetch('http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D');
+            const response = await fetch('https://mocki.io/v1/36c32f7c-dee4-4a4a-a093-b3a9aae0c0bc');
             
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
